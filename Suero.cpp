0f19cd46622bb993265ec4f3d0c76dce7fca592a
LucasWaisten <43937195+LucasWaisten@users.noreply.github.com>
2019-10-20 12:43:02 -0300

    Add files via upload

--- Suero.cpp
+++ Suero.cpp
@@ -1,11 +1,25 @@
 #include "Suero.h"
 
-Suero::Suero()
-{
-    //ctor
+Suero::Suero(string, int){
+    set_tipo(string);
+    set_cantidad(int);
 }
 
-Suero::~Suero()
-{
-    //dtor
+Suero::~Suero(){
+    set_tipo();
+    set_cantidad(0);
+}
+void Suero::set_tipo(string t){
+    tipo = t;
+}
+void Suero::set_cantidad(int c){
+    cantidad = c;
+}
+
+string Suero::get_tipo(){
+    return tipo;
+}
+
+int Suero::get_cantidad(){
+    return cantidad;
 }
--- Suero.h
+++ Suero.h
@@ -1,16 +1,22 @@
+#include "Elementos.h"
 #ifndef SUERO_H
 #define SUERO_H
 
 
-class Suero
+
+class Suero: public Elemento
 {
     public:
         Suero(string tipo, int cantidad);
+        void set_tipo(string);
+        void set_cantidad(int);
+        string get_tipo()
+        int get_cantidad()
         virtual ~Suero();
 
-    protected:
-
     private:
+        string tipo;
+        int cantidad;
 };
 
 #endif // SUERO_H
